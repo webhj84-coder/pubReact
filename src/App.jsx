4362aa8e61cc91fabd218676ccb902c59c727e37
webhj84 <webhj84@gmail.com>
2026-03-14 11:01:13 +0900

    scss적용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // 레이아웃
 import PubLayout from './layouts/PubLayout';
 
-// 페이지\
+// 페이지
 import WorkList from './pages/pub/WorkList';
 import GuideIntro from './pages/pub/GuideIntro';
 import GuideButton from './pages/pub/GuideButton';
+// import GuideList from './pages/pub/GuideList'; // /guide/list용 컴포넌트가 있다면 임포트 필요
 
 function App() {
-	return(
-		<BrowserRouter>
-			<Routes>
-				<Route path="/pub" element={<PubLayout />}>
-					{/* 작업목록 */}
-					<Route index element={<Navigate to="/list" replace />} />
-					<Route path="list" element={<WorkList />}/>
+  return(
+    // GitHub Pages 정적 호스팅 환경에 맞춰 HashRouter 사용 (basename 불필요)
+    <HashRouter>
+      <Routes>
+        <Route path="/pub" element={<PubLayout />}>
+          {/* 작업목록 */}
+          {/* 절대 경로(/list)가 아닌 상대 경로(list) 사용 */}
+          <Route index element={<Navigate to="list" replace />} />
+          <Route path="list" element={<WorkList />}/>
 
-					{/* 가이드 */}
-					<Route path="guide" element={<Navigate to="intro" replace />} />
-					<Route path="guide/intro" element={<GuideIntro />}/>
-					<Route path="guide/button" element={<GuideButton />}/>
+          {/* 가이드 */}
+          {/* 절대 경로가 아닌 상대 경로(intro) 사용 */}
+          <Route path="guide" element={<Navigate to="intro" replace />} />
+          <Route path="guide/intro" element={<GuideIntro />}/>
+          <Route path="guide/button" element={<GuideButton />}/>
+          
+          {/* 사용자가 접근하려는 guide/list 경로가 필요하다면 아래와 같이 추가해야 합니다. */}
+          {/* <Route path="guide/list" element={<GuideList />}/> */}
 
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	)
+        </Route>
+      </Routes>
+    </HashRouter>
+  )
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pubReact/', // 이 줄을 반드시 추가해 주세요.
 })
